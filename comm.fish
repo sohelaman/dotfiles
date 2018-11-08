ac8f1f7ea7b6428f2bbfb5bcb9fb8b8f4bb3b8eb
Sohel Aman <sohelamankhan@gmail.com>
2018-11-08 11:39:26 +0600

    Seperated alias list

--- comm.fish
+++ comm.fish
@@ -5,6 +5,7 @@ export EDITOR=vim
 
 source ~/.shellinc/fish/fish_env_arch.fish
 source ~/.shellinc/fish/fish_paths.fish
+source ~/.shellinc/fish/fish_aliases.fish
 source ~/.shellinc/fish/fish_functions.fish
 source ~/.shellinc/fish/fish_functions_git.fish
 source ~/.shellinc/fish/fish_functions_misc.fish
