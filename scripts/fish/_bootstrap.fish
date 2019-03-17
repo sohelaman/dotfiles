45dc12234e0a78f131045a4f5c007ca7786005e6
Sohel Aman <sohelamankhan@gmail.com>
2019-03-17 17:27:03 +0600

    Updated scripts

--- scripts/fish/_bootstrap.fish
+++ scripts/fish/_bootstrap.fish
@@ -7,3 +7,4 @@ source $scripts_root/fish/fish_aliases.fish
 source $scripts_root/fish/fish_functions.fish
 source $scripts_root/fish/fish_functions_git.fish
 source $scripts_root/fish/fish_functions_misc.fish
+
--- scripts/scripts-bootstrap.fish
+++ scripts/scripts-bootstrap.fish
@@ -2,7 +2,8 @@
 # FOR FISH SHELL ONLY
 
 export EDITOR=vim
-
-set scripts_root '~/.dotfiles/scripts'
+set -U scripts_root ~/.dotfiles/scripts
 
 source $scripts_root/fish/_bootstrap.fish
+#source ~/.dotfiles/scripts/fish/_bootstrap.fish
+
