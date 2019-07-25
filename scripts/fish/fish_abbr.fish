90001a29a90efcc42566fb98cfe9894781eca364
Sohel Aman <sohelamankhan@gmail.com>
2019-07-25 09:04:40 +0000

    Docker abbr.

--- scripts/fish/fish_abbr.fish
+++ scripts/fish/fish_abbr.fish
@@ -9,8 +9,13 @@ abbr -a pac sudo pacman -S
 abbr -a apt sudo apt install
 abbr -a catssh 'cat ~/.ssh/id_rsa.pub | ssh USER@HOST "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"'
 abbr -a sd sudo systemctl
-abbr -a d sudo docker
-abbr -a dc sudo docker-compose
+
+# Docker
+abbr -a d docker
+abbr -a di docker images
+abbr -a dc docker-compose
+abbr -a dcu docker-compose up -d
+abbr -a dcd docker-compose down
 
 # Git
 abbr -a g git
