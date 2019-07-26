d7eceeb9e090eafa174f28fd5fe8b38f4189699d
Sohel Aman <sohelamankhan@gmail.com>
2019-07-26 10:34:20 +0000

    i3blocks date blocklet clicks

--- scripts/i3blocks/blocklets/calender.sh
+++ scripts/i3blocks/blocklets/calender.sh
@@ -3,8 +3,19 @@
 # clickable calendar/date
 
 strdate=$(date '+%Y-%m-%d')
+today="Today: $(date '+%A, %B %d, %Y')"
 
 case $BLOCK_BUTTON in
-  3) echo "$strdate" && zenity --calendar ;; # right-click
+  # left click
+  1) echo "$strdate" && date '+%Y-%m-%d_%H-%M-%S' | tr -d '\n' | xsel -b;;
+  # middle click
+  2) echo "$strdate" && date '+%Y-%m-%d %H:%M:%S' | tr -d '\n' | xsel -b ;;
+  # mousewheel up
+  4) echo "$strdate" && date '+%Y-%m-%d_%H%M%S' | tr -d '\n' | xsel -b ;;
+  # mousewheel down
+  5) echo "$strdate" && date '+%Y-%m-%d_%H%M%S' | tr -d '\n' | xsel -b ;;
+  # right click
+  3) echo "$strdate" && zenity --calendar --date-format=%Y-%m-%d --text="$today" | tr -d '\n' | xsel -b ;;
+  # any mouse button
   *) echo "$strdate" ;;
 esac
